09306af8b1c1f879502a80404c94f0ea96058343
Hannahvic <hannahvl1987@hotmail.com>
2020-07-25 10:51:37 +0100

    updated - ES6

--- script.js
+++ script.js
@@ -1,111 +1,114 @@
-var getUpTime = 7;
-var noon = 12;
-var lunchTime = 14;
-var dinnerTime = 18;
-var nightTime = 22;
-var partyTime;
+{
+  let getUpTime = 7;
+  let noon = 12;
+  let lunchTime = 14;
+  let dinnerTime = 18;
+  let nightTime = 22;
+  let partyTime;
 
-var showCurrentTime = function () {
-  var clock = document.getElementById('clock');
+  let showCurrentTime = function () {
+    let clock = document.getElementById('clock');
 
-  var currentTime = new Date();
+    let currentTime = new Date();
 
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-  var meridian = 'AM';
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let meridian = 'AM';
 
-  if (hours >= noon) {
-    meridian = 'PM';
-  }
-  if (hours > noon) {
-    hours = hours - 12;
-  }
-  if (minutes < 10) {
-    minutes = '0' + minutes;
-  }
-  if (seconds < 10) {
-    seconds = '0' + seconds;
-  }
+    if (hours >= noon) {
+      meridian = 'PM';
+    }
+    if (hours > noon) {
+      hours = hours - 12;
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
 
-  var clockTime = hours + ':' + minutes + ':' + seconds + ' ' + meridian + '!';
+    let clockTime =
+      hours + ':' + minutes + ':' + seconds + ' ' + meridian + '!';
 
-  clock.innerText = clockTime;
-};
+    clock.innerText = clockTime;
+  };
 
-var updateClock = function () {
-  var time = new Date().getHours();
-  var messageText;
-  var image = './assets/daytime.jpg';
+  let updateClock = function () {
+    let time = new Date().getHours();
+    let messageText;
+    let image = './assets/daytime.jpg';
 
-  var timeEventJS = document.getElementById('timeEvent');
-  var generalImageJS = document.getElementById('generalImage');
+    let timeEventJS = document.getElementById('timeEvent');
+    let generalImageJS = document.getElementById('generalImage');
 
-  if (time == partyTime) {
-    image = './assets/party.jpg';
-    messageText = 'Time to party!';
-  } else if (time == getUpTime) {
-    image = './assets/wakeUp.jpg';
-    messageText = 'Get up!';
-  } else if (time == lunchTime) {
-    image = './assets/lunch.png';
-    messageText = 'Are you hungry?';
-  } else if (time == dinnerTime) {
-    image = './assets/dinner.png';
-    messageText = 'Dinnertime!';
-  } else if (time == nightTime) {
-    image = './assets/night.jpg';
-    messageText = 'Go to bed!';
-  } else {
-    image = './assets/daytime.jpg';
-    messageText = 'Have a good day!';
-  }
-  console.log(messageText);
-  timeEventJS.innerText = messageText;
-  generalImageJS.src = image;
+    if (time == partyTime) {
+      image = './assets/party.jpg';
+      messageText = 'Time to party!';
+    } else if (time == getUpTime) {
+      image = './assets/wakeUp.jpg';
+      messageText = 'Get up!';
+    } else if (time == lunchTime) {
+      image = './assets/lunch.png';
+      messageText = 'Are you hungry?';
+    } else if (time == dinnerTime) {
+      image = './assets/dinner.png';
+      messageText = 'Dinnertime!';
+    } else if (time == nightTime) {
+      image = './assets/night.jpg';
+      messageText = 'Go to bed!';
+    } else {
+      image = './assets/daytime.jpg';
+      messageText = 'Have a good day!';
+    }
+    console.log(messageText);
+    timeEventJS.innerText = messageText;
+    generalImageJS.src = image;
 
-  showCurrentTime();
-};
-updateClock();
+    showCurrentTime();
+  };
+  updateClock();
 
-var oneSecond = 1000;
-setInterval(updateClock, oneSecond);
+  let oneSecond = 1000;
+  setInterval(updateClock, oneSecond);
 
-const partyButton = document.getElementById('partyTimeButton');
+  const partyButton = document.getElementById('partyTimeButton');
 
-const partyEvent = function () {
-  if (partyTime < 0) {
-    partyTime = new Date().getHours();
-    partyTimeButton.innerText = 'Party Over!';
-  } else {
-    partyTime = -1;
-    partyTimeButton.innerText = 'Party Time!';
-  }
-};
+  const partyEvent = function () {
+    if (partyTime < 0) {
+      partyTime = new Date().getHours();
+      partyTimeButton.innerText = 'Party Over!';
+    } else {
+      partyTime = -1;
+      partyTimeButton.innerText = 'Party Time!';
+    }
+  };
 
-partyButton.addEventListener('click', partyEvent);
-partyEvent();
+  partyButton.addEventListener('click', partyEvent);
+  partyEvent();
 
-var getUpSelect = document.getElementById('getUpSelect');
+  let getUpSelect = document.getElementById('getUpSelect');
 
-var getUpEvent = function () {
-  getUpTime = getUpSelect.value;
-};
+  let getUpEvent = function () {
+    getUpTime = getUpSelect.value;
+  };
 
-getUpSelect.addEventListener('change', getUpEvent);
+  getUpSelect.addEventListener('change', getUpEvent);
 
-var lunchTimeSelect = document.getElementById('lunchTimeSelect');
+  let lunchTimeSelect = document.getElementById('lunchTimeSelect');
 
-var lunchTimeEvent = function () {
-  lunchTime = lunchTimeSelect.value;
-};
+  let lunchTimeEvent = function () {
+    lunchTime = lunchTimeSelect.value;
+  };
 
-lunchTimeSelect.addEventListener('change', lunchTimeEvent);
+  lunchTimeSelect.addEventListener('change', lunchTimeEvent);
 
-var dinnerTimeSelect = document.getElementById('dinnerTimeSelect');
+  let dinnerTimeSelect = document.getElementById('dinnerTimeSelect');
 
-var dinnerTimeEvent = function () {
-  dinnerTime = dinnerTimeSelect.value;
-};
+  let dinnerTimeEvent = function () {
+    dinnerTime = dinnerTimeSelect.value;
+  };
 
-dinnerTimeSelect.addEventListener('change', dinnerTimeEvent);
+  dinnerTimeSelect.addEventListener('change', dinnerTimeEvent);
+}
